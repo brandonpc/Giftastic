1a4878308525c26b5a4431981a22e1411d871f14
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-03-20 00:20:03 -0600

    mostly done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,75 @@
 // let's get started!
-$(function)() {
-
-}
+$(function () {
+    makeButtons(topics, "searchButton", "#gif-buttons")
+    console.log("First Function: Did it work?")
+});
 var topics = ["zelda", "nintendo", "video games", "captain america", "marvel", "avengers"];
 
 // function to create a button for each string in topics
 //      for loop to append a button
 function makeButtons() {
-    for (var i = 0; i < topics.length; i++)
+    $("#gif-buttons").empty();
+
+    for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
-    // Adding a class to our button
-    a.addClass("gif-button");
-    // Adding a data-attribute
-    a.attr("data-name", topics[i]);
-    // Providing the initial button text
-    a.text(topics[i]);
-    // Append
-    $("#buttons-view").append(a);
+        // Adding a class to our button
+        a.addClass("gif-buttons");
+        // Adding a data-attribute
+        a.attr("data-name", topics[i]);
+        // Providing the initial button text
+        a.text(topics[i]);
+        // Append
+        $("#gif-buttons").append(a);
+        console.log("Yay! you called makeButtons!!");
+    }
+
+    console.log(a);
 }
 
-// function topicInfo() {
-//     var bcApiKey = "Q4S0rOrLTGQHSEql1EOhvcM6Fi2S5UQi";
-//     var topicVar = $(this).attr("data-name");
-//     var queryURL = `https://api.giphy.com/v1/gifs/search/random?api_key=${bcApiKey}&limit=10&offset=0&rating=G&lang=en`;
+$(document).on('click', '.searchButton', function () {
+    var dataType = $(this).data('type');
+    var bcApiKey = "Q4S0rOrLTGQHSEql1EOhvcM6Fi2S5UQi";
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${dataType}api_key=${bcApiKey}&limit=10`;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (var i = 0; i < topics.length; i++) {
+            var gifresponseDiv = response.data[i].rating;
+            var rating = response.data[i].rating;
+            var ratingText = $('<p>').text("Rating: " + ratingText);
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log("Something should be here")
-//         console.log(response);
-//         var buttonDiv = $("<div class='button'>");
-//         //      ***MAKE DA GIF IMG***
-//         // var gifURL = response.????
-//         // var gifPic = $("<img>").attr("src", )
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var gifImage = $('<img>');
+            gifImage.addClass("clickable-gif");
+            gifImage.attr("src", still);
+            gifImage.attr("data-still", still);
+            gifImage.attr("data-animated", animated);
+            gifImage.attr("data-state", "still");
+            $("#new-gifs").append(searchDiv);
+        }
 
-//         //      ***MAKE DA RATING***
-//         // var rating = response.(what???)
-//         // var ratingText = $("<p>").text("Rating: " + rating);
-//         // movieDiv.append(ratingText);
-//         $("#buttons-view").prepend(movieDiv);
+    });
+});
 
+$(document).on('click', '.Image', function () {
+    var gifState = $(this).data('state');
+    if (gifState === 'still') {
+        $(this).attr('src', $(this).data('animated'));
+        $(this).attr('data-state', 'animated');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+});
 
-//     })
+// $(document).on("click", ".gif-button", displayTopicInfo);
 
-// }
-// topicInfo();
+// makeButtons();
+
+// 
 
 // on click(button): 10 static, not animated images from giphy API
 //      every gif should display: rating
